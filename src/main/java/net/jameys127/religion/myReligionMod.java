9efbc6a5c6e3db9cc3587fc9bc0a8712fd29c14d
Jamey <jeremiah_sheehy1@my.vcccd.edu>
2024-05-23 21:27:42 -0700

    Initial commit

---
+++ src/main/java/net/jameys127/religion/myReligionMod.java
@@ -0,0 +1,57 @@
+package net.jameys127.religion;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(myReligionMod.MODID)
+public class myReligionMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "religion";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public myReligionMod(){
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+        modEventBus.addListener(this::commonSetup);
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+
+    }
+
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event){
+
+    }
+
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents{
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+
+        }
+    }
+}
